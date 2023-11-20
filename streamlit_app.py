6a9945e86bcebfe3cc46b6690e57f2ab7e7db7d4
wolowizard3 <113815026+wolowizard3@users.noreply.github.com>
2023-11-20 15:48:28 +0800

    Update streamlit_app.py
    added header
    added sub-header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+# Breakfast menu
+streamlit.header('Breakfast Menu')
+
+# List 1
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+# List 2
+streamlit.subheader('Kale, Spinach & Rocket Smoothie')
+# List 3
+streamlit.subheader('Hard-Boiled Free Ranged Egg')
