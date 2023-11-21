0d3de08e1762aa9ea8d0db4823377962d8a7b019
wolowizard3 <113815026+wolowizard3@users.noreply.github.com>
2023-11-21 17:13:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,6 @@ streamlit.dataframe(my_data_rows)
 #allow end-user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#this will not work correctly, but just go wuth the flow
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
