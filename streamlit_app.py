4509ba80a86ba507231cc70d93808418b6f3f7ad
wolowizard3 <113815026+wolowizard3@users.noreply.github.com>
2023-11-21 17:01:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 #allow end-user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('Thanks for adding ', fruit_choice)
